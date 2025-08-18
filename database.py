31aece87966463b9588efbe82bcb0007da020252
Joan Mendoza <joanmendoza51015@gmail.com>
2025-08-17 21:12:45 -0500

    Fix Update 1

--- database.py
+++ database.py
@@ -8,19 +8,24 @@ def create_connection():
     
     try:
         connection = mysql.connector.connect(
-            host=os.getenv('HOSTNAME'),
+            host=os.getenv('DB_HOST'),
             port=int(os.getenv('DB_PORT', 3306)),
-            user=os.getenv('USERNAME'),
-            password=os.getenv('PASSWORD'),
-            database=os.getenv('DATABASE')
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            database=os.getenv('DB_NAME'),
+            ssl_ca=os.getenv("SSL_CA_PATH"),
+            ssl_disabled=False 
         )
         return connection
     except Error as e:
         print(f"Error connecting to MySQL: {e}")
         return None
 
-def close_connection(connection):
-    if connection and connection.is_connected():
-        connection.close()
-
-
+def close_connection(connection, cursor=None):
+    try:
+        if cursor:
+            cursor.close()
+        if connection and connection.is_connected():
+            connection.close()
+    except:
+        pass
